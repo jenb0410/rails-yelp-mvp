249f1aa999fc0c61403da0b30722914345a40c92
Jennifer Braun <jenb04100@gmail.com>
2023-02-22 17:02:54 +0100

    lalala

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,22 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # # A visitor can see the list of all restaurants.
+  # get 'restaurants', to: 'restaurants#index'
+
+  # # A visitor can add a new restaurant, and be redirected to the show view of that new restaurant.
+  # get 'restaurants/new', to: 'restaurants#new', as: :new_restaurant
+  # post 'restaurants', to: 'restaurants#create'
+
+  # # A visitor can see the details of a restaurant, with all the reviews related to the restaurant.
+  # get 'restaurants/:id', to: 'restaurants#show', as: :restaurant
+
+  # # A visitor can add a new review to a restaurant
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new', as: :new_review
+  # post 'restaurants/:id/reviews', to: 'reviews#create'
+  get 'reviews/new'
+  resources :restaurants do
+    resources :reviews, only: [:new]
+  end
 end
